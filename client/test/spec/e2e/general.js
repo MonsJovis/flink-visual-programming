a32bd150112222740a3da142ee84b5c4fbe4149e
MonsJovis <markus.aurich@gmail.com>
2016-02-10 10:30:20 +0100

    implement some basic e2e tests (not finished yet)

--- client/test/spec/e2e/general.js
+++ client/test/spec/e2e/general.js
@@ -10,3 +10,33 @@ describe('Flink Visual App', function() {
     });
 
 });
+
+describe('Executing the Word-Count by clicking on Deploy & Run', function() {
+
+    it('should have a OK result', function() {
+        browser.get(appDomain);
+        element(by.css('.navbar-btn-deploy-run')).click();
+        // TODO: check an DOM element for the result
+    });
+
+});
+
+describe('Executing the Word-Count by clicking on Code', function() {
+
+    it('should have a OK result', function() {
+        browser.get(appDomain);
+        element(by.css('.navbar-btn-download-code')).click();
+        // TODO: check an DOM element for the result
+    });
+
+});
+
+describe('Executing the Word-Count by clicking on Jar', function() {
+
+    it('should have a OK result', function() {
+        browser.get(appDomain);
+        element(by.css('.navbar-btn-download-jar')).click();
+        // TODO: check an DOM element for the result
+    });
+
+});
